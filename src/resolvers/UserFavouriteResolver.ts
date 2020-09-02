ae4ad0e32ea4f9344a287e5e5f31298ce4a54f49
Darren Keen <darren.keen@fanduel.com>
2020-09-03 00:05:32 +0100

    Updated user favourite

--- src/resolvers/UserFavouriteResolver.ts
+++ src/resolvers/UserFavouriteResolver.ts
@@ -8,7 +8,11 @@ import {
   Int,
   UseMiddleware,
   Ctx,
+  ArgsType,
+  Args,
 } from 'type-graphql';
+import { Repository } from 'typeorm';
+import { InjectRepository } from 'typeorm-typedi-extensions';
 import { ApolloError } from 'apollo-server-express';
 
 import { MyContext } from './../graphql-types/MyContext';
@@ -24,20 +28,38 @@ class UserFavouriteInput {
   resourceId: number;
 }
 
+@ArgsType()
+class GetUserFavouriteArgs {
+  @Field(() => Int, { nullable: true })
+  resourceId?: number;
+
+  @Field(() => Int, { nullable: true })
+  userId?: number;
+}
+
 @Resolver()
 export class UserFavouriteResolver {
+  constructor(
+    @InjectRepository(UserFavourite)
+    private readonly userFavouriteRepo: Repository<UserFavourite>,
+    @InjectRepository(Resource)
+    private readonly resourceRepo: Repository<Resource>,
+    @InjectRepository(User)
+    private readonly userRepo: Repository<User>
+  ) {}
+
   @Mutation(() => UserFavourite)
   @UseMiddleware(isAuth)
   async createUserFavourite(
     @Ctx() ctx: MyContext,
     @Arg('options', () => UserFavouriteInput) options: UserFavouriteInput
   ) {
-    const user = await User.findOne(ctx.req.userId);
+    const user = await this.userRepo.findOne(ctx.req.userId);
     if (!user) {
       throw new ApolloError('No user');
     }
 
-    const resource = await Resource.findOne(options.resourceId);
+    const resource = await this.resourceRepo.findOne(options.resourceId);
     if (!resource) {
       throw new ApolloError('No resource');
     }
@@ -46,32 +68,40 @@ export class UserFavouriteResolver {
       user,
       resource,
     };
-    const userFavourite = await UserFavourite.create(data).save();
+    const userFavourite = await this.userFavouriteRepo.create(data).save();
     return userFavourite;
   }
 
-  // @Mutation(() => Boolean)
-  // @UseMiddleware(isAuth)
-  // async updateUserRole(
-  //   @Arg('id', () => Int) id: number,
-  //   @Arg('input', () => UserRoleInput) input: UserRoleInput
-  // ) {
-  //   await UserRole.update({ id }, input);
-  //   return true;
-  // }
+  @Mutation(() => Boolean)
+  @UseMiddleware(isAuth)
+  async deleteUserFavourite(
+    @Ctx() ctx: MyContext,
+    @Arg('id', () => Int) id: number
+  ) {
+    const userFavourite = await this.userFavouriteRepo.findOne(id, {
+      where: { user: { id: ctx.req.userId } },
+    });
+    if (!userFavourite) {
+      throw new ApolloError('No matching favourite found');
+    }
 
-  // @Mutation(() => Boolean)
-  // @UseMiddleware(isAuth)
-  // async deleteUserRole(@Arg('id', () => Int) id: number) {
-  //   const res = await UserRole.delete({ id });
-  //   if (res.affected) {
-  //     return true;
-  //   }
-  //   return false;
-  // }
+    const res = await this.userFavouriteRepo.delete({ id });
+    if (res.affected) {
+      return true;
+    }
+    return false;
+  }
 
   @Query(() => [UserFavourite])
-  userFavourite() {
-    return UserFavourite.find({ relations: ['user', 'resource'] });
+  userFavourite(@Args() { resourceId, userId }: GetUserFavouriteArgs) {
+    const where: {
+      resource?: number;
+      user?: number;
+    } = {
+      ...(resourceId && { resource: resourceId }),
+      ...(userId && { user: userId }),
+    };
+
+    return UserFavourite.find({ relations: ['user', 'resource'], where });
   }
 }
